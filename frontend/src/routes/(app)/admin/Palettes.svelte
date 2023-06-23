8b816e0590b8e4dbd5b7ec13e8e36cba78d92de4
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2023-06-23 16:25:17 -0300

    Adds skin images and search bar in palette choices

--- frontend/src/routes/(app)/admin/Palettes.svelte
+++ frontend/src/routes/(app)/admin/Palettes.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
-	import { Input, Dropdown, DropdownItem, Tooltip, Button, Heading } from 'flowbite-svelte';
+	import { Input, Dropdown, DropdownItem, Tooltip, Button, Heading, Search } from 'flowbite-svelte';
 
 	export let palettes: Array<{ name: string; skin: string[]; default: boolean }>;
 	export let diceSkinOptions: string[];
+
+	let searchTerm = '';
 
 	const createNewPalette = (name: string) => ({
 		name: name,
@@ -22,9 +24,13 @@
 
 	const setDefaultPalette = (paletteIndex: number) => {
 		palettes = palettes.map((palette, index) => {
-            palette.default = index === paletteIndex;
+			palette.default = index === paletteIndex;
 			return palette;
 		});
+	};
+
+	const skinUrl = (skin: string, number: number, type: string = 'd10') => {
+		return `https://assets.togarashi.app/dice/${type}/${skin}/${number}.png`;
 	};
 </script>
 
@@ -34,7 +40,7 @@
 	<div class="flex items-center my-2">
 		<Input
 			label="Palette name"
-			class="w-72 mr-1"
+			class="w-72 mr-1 self-stretch"
 			placeholder="Palette name"
 			bind:value={palette.name}
 			color={palette.name ? 'base' : 'red'}
@@ -43,12 +49,30 @@
 
 		<div class="grid w-full gap-1 grid-cols-4 md:grid-cols-6 lg:grid-cols-12">
 			{#each palette.skin as skinName, skinIndex}
-				<Button color="light">{skinName}</Button>
-				<Dropdown>
-					{#each diceSkinOptions as diceSkinName}
-						<DropdownItem on:click={chooseSkin(paletteIndex, skinIndex, diceSkinName)}>
-							{diceSkinName}
-						</DropdownItem>
+				<Button color="light">
+					<img
+						src={skinUrl(skinName, (skinIndex + 1) % palette.skin.length)}
+						class="w-12"
+						alt="{skinName} dice"
+					/>
+				</Button>
+				<Dropdown class="overflow-y-auto max-h-96">
+					<div class="p-2">
+						<Search bind:value={searchTerm} size="md" />
+					</div>
+					{#each diceSkinOptions.sort() as diceSkinName}
+						{#if diceSkinName.toLowerCase().includes(searchTerm.toLowerCase())}
+							<DropdownItem on:click={chooseSkin(paletteIndex, skinIndex, diceSkinName)}>
+								<div class="flex items-center">
+									<img
+										src={skinUrl(diceSkinName, (skinIndex + 1) % palette.skin.length)}
+										class="w-24 mr-2"
+										alt="{skinName} dice"
+									/>
+									<span class="flex-1">{diceSkinName}</span>
+								</div>
+							</DropdownItem>
+						{/if}
 					{/each}
 				</Dropdown>
 			{/each}
