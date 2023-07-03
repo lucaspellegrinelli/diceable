6d737da949ead10a5778661fbb6a4b7939839d0f
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2023-07-02 22:22:25 -0300

    Revert "Moves build target to Cloudflare (#22)"
    This reverts commit b223eceee7d5377a8393fce5259aa449959bf140.

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -17,13 +17,7 @@ const config = {
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
         // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-        adapter: adapter({
-            // See below for an explanation of these options
-            routes: {
-                include: ['/*'],
-                exclude: ['<all>']
-            }
-        })
+        adapter: adapter(),
     }
 };
 
