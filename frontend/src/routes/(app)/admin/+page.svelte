ca18c9e53781c5e94af254a60a23713846f1a483
Lucas Pellegrinelli <lucas.r.pellegrinelli@gmail.com>
2023-10-06 12:12:00 -0300

    Adds simple homepage for /admin

---
+++ frontend/src/routes/(app)/admin/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { Button } from 'flowbite-svelte';
+</script>
+
+<Button color="blue" href="/admin/d10" pill>
+    Go to d10
+</Button>
+
+<Button color="blue" href="/admin/d20" pill>
+    Go to d20
+</Button>
