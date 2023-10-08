d134947faa2e91d5e06a65085601ae55f203c991
Lucas Pellegrinelli <lucas.r.pellegrinelli@gmail.com>
2023-10-08 11:51:48 -0300

    Fixed hot reloading

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     server: {
-        port: parseInt(process.env.PORT || "3000")
+        port: parseInt(process.env.PORT || "3000"),
+        watch: {
+            usePolling: true,
+            interval: 100
+        }
     },
     preview: {
         port: parseInt(process.env.PORT || "3000")
