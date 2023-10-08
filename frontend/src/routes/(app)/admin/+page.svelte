24b60aa3f959a232361d4bc06920947cfb7cca9f
Lucas Pellegrinelli <lucas.r.pellegrinelli@gmail.com>
2023-10-08 12:02:10 -0300

    Added prettier side choice

--- frontend/src/routes/(app)/admin/+page.svelte
+++ frontend/src/routes/(app)/admin/+page.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
-	import { Button } from 'flowbite-svelte';
+	import { Card, Heading } from 'flowbite-svelte';
 </script>
 
-<Button color="blue" href="/admin/d10" pill>
-    Go to d10
-</Button>
+<div class="p-8">
+    <Heading class="mb-6" tag="h2">which dice to configure</Heading>
 
-<Button color="blue" href="/admin/d20" pill>
-    Go to d20
-</Button>
+    <div class="grid w-full grid-cols-4 gap-4">
+        <Card img="https://assets.togarashi.app/dice/d10/indigo/0.png" href="/admin/d10" horizontal>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">D10</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">Ten-sided dice</p>
+        </Card>
+
+        <Card img="https://assets.togarashi.app/dice/d20/indigo/0.png" href="/admin/d20" horizontal>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">D20</h5>
+            <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">Twenty-sided dice</p>
+        </Card>
+    </div>
+</div>
