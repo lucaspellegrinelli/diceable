401760d09cd683f8222645a4bf60b1e4ea98da85
Lucas Pellegrinelli <lucas.pellegrinelli@hotmail.com>
2023-12-27 18:23:40 -0300

    Added dark mode

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -1,10 +1,13 @@
 <script>
+	import DarkModeToggle from '$lib/components/DarkModeToggle.svelte';
 	import '../../app.pcss';
+	import { ModeWatcher } from 'mode-watcher';
 </script>
 
 <svelte:head>
 	<title>Diceable</title>
 </svelte:head>
 
-<!-- <ToastHandler /> -->
+<DarkModeToggle />
+<ModeWatcher />
 <slot />
