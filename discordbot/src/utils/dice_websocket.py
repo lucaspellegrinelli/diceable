2c60684671ef264e3524916f19bee5005d10e8b6
Lucas Pellegrinelli <lucas.r.pellegrinelli@gmail.com>
2025-06-16 17:09:55 -0300

    Fix websocket method

--- discordbot/src/utils/dice_websocket.py
+++ discordbot/src/utils/dice_websocket.py
@@ -10,7 +10,7 @@ class DiceWebSocketPublisher:
         Args:
             websocket_host: The host of the dice WebSocket service (e.g., "ws-proxy:3000")
         """
-        self.host = f"http://{websocket_host}"
+        self.host = f"https://{websocket_host}"
         self.logger = logging.getLogger("diceable-discord-bot")
 
     def publish_roll(self, user_id: str, roll_data: dict):
